810eff5b800c3115aac4beefa60f25466d75eb49
FengFan <mail@fengfan.me>
2019-10-08 19:15:49 +0800

    feat(footer): update desc

--- src/components/crulse/footer.tsx
+++ src/components/crulse/footer.tsx
@@ -2,7 +2,7 @@ import { VNode } from '@/lib/model'
 import MiniReact from '@/react/index'
 export default class Footer extends MiniReact.Component {
     public state = {
-        copyright: '@Copyright 2019 Oct16'
+        copyright: '@Copyright 2019 oct16'
     }
     public render(): VNode {
         return <footer className="cruise-footer">{this.state.copyright}</footer>
