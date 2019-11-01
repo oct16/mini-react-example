648e4c038f243df35f93704fb2809e3a14166bbd
FengFan <mail@fengfan.me>
2019-11-01 11:24:22 +0800

    test: add test

---
+++ src/test/add.test.ts
@@ -0,0 +1,10 @@
+function add(x: number, y: number) {
+    return x + y
+}
+import { expect } from 'chai'
+
+describe('should ....', () => {
+    it('1 + 1 equal 2', () => {
+        expect(add(1, 1)).to.be.equal(2)
+    })
+})
